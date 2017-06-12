ba10c4388e41bac141152925a52aa3ca1b0f3e15
Cameron Gorrie <sastraxi@gmail.com>
2017-06-11 21:12:07 -0400

    start service from the list

--- components/ServiceList.js
+++ components/ServiceList.js
@@ -2,18 +2,30 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
 import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import { startService } from '../actions';
 
-const Service = ({ name, service }) =>
-  <Fabric>
-    <DefaultButton>{name}</DefaultButton>
-    <p>This service has {service.exec.length} execution plan(s).</p>
-    <p>This service is {service.running || 'not'} running.</p>
-  </Fabric>;
+const mapDispatchToProps = dispatch => ({
+  startServiceByName: name => dispatch(startService(name)),
+});
+
+const Service = connect(null, mapDispatchToProps)(
+  ({ name, service, startServiceByName }) =>
+    <Fabric>
+      <DefaultButton>{name}</DefaultButton>
+      <p>This service has {service.exec.length} execution plan(s).</p>
+      <p>This service is {service.running || 'not'} running.</p>
+      <DefaultButton onClick={() => startServiceByName(name)}>
+        Start
+      </DefaultButton>
+    </Fabric>,
+  );
 
 Service.propTypes = {
   name: React.PropTypes.string.isRequired,
   service: React.PropTypes.object.isRequired, // eslint-disable-line
+  startServiceByName: React.PropTypes.func.isRequired,
 };
+
 
 // services is a map from name => descriptor/status
 const ServiceList = ({ services }) => (
